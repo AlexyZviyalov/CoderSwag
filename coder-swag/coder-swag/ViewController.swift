d700a9b879f0c0d9c06b7b4f2c16e0799c68f2b8
ALIAKSEI ZAUYALAU <aliakseizauyalau@MacBook-Pro-ALIAKSEI.local>
2020-01-09 20:18:21 +0300

    initial commit

---
+++ coder-swag/coder-swag/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  coder-swag
+//
+//  Created by ALIAKSEI ZAUYALAU on 1/9/20.
+//  Copyright © 2020 ALIAKSEI ZAUYALAU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
